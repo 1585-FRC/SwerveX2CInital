cee80639f1d2312f3c4a2a529c3049b61ed8c2a4
RJRobotics <>
2026-02-12 16:38:57 -0500

    updated with pathplanner

---
+++ src/main/java/frc/robot/subsystems/intake.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class intake extends SubsystemBase{
+    public intake(int mototrchannel){
+
+        //m_motorGroupA = new SparkMax(mototrchannel, SparkLowLevel.MotorType.kBrushed);
+    }
+    
+}
