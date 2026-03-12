b447af9d322f20c88566477734d31d3f4de60c6d
RJRobotics <>
2026-03-12 14:23:17 -0400

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -41,6 +41,6 @@ public class Constants {
         public static final int ELEVATOR_MOTOR_ID = 61;
         public static final int ANALOG_POTENTIOMETER_CHANNEL = 0;
         public static final boolean ELEVATOR_DEBUG_ENABLED = false;
-        public static final double ELEVATOR_LIMIT_SWITCH_THRESHOLD = 0.19;
+        public static final double ELEVATOR_LIMIT_SWITCH_THRESHOLD = 0.23;
     }
 }
